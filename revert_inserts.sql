15e833c2f78351c0b9f2c81925e03554439de0d7
Ivana <iv.nencheva@gmail.com>
2014-12-17 00:21:34 +0000

    dummy queries to revert all inserts

---
+++ revert_inserts.sql
@@ -0,0 +1,4 @@
+delete from feed_free_votes_domande where id >= 20;
+delete from feed_free_votes_risposte where id >= 50;
+delete from feed_modules where id >= 1025;
+delete from feed_content where id >= 1025;
